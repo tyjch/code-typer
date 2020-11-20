515c1ae5d6031029e619bc8e4d882f4ea77a9b98
Tyler Churchill <tyjchurchill@gmail.com>
2020-11-20 15:52:02 -0800

    Typewriter effect that changes diffs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState, useImperativeHandle} from "react";
+import TypeIt from "typeit";
+import DiffMatchPatch from "diff-match-patch";
+import './App.css'
 
-function App() {
+
+const differ = new DiffMatchPatch();
+
+const CodeTyper = React.forwardRef(({strings}, ref) => {
+
+  const codeRef = useRef(null);
+  const typeRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    step() {
+      console.log('`step()` called from imperative handle')
+      if (typeRef.current !== null) {
+        typeRef.current.unfreeze();
+      }
+    }
+  }),)
+
+  useEffect(() => {
+    typeRef.current = new TypeIt(codeRef.current, {
+      speed       : 50,
+      // afterStep : (step, instance) => {
+      //   console.log(step)
+      // }
+    });
+    console.log('change this')
+    // typeRef.current = typeRef.current.empty();
+    let prev = '';
+
+    for (const curr of strings) {
+      typeRef.current = typeRef.current.move('START');
+      let diff        = differ.diff_main(prev, curr);
+
+      for (const d of diff) {
+        const [op, text] = d;
+        switch (op) {
+          case  0 : {
+            typeRef.current = typeRef.current.move(text.length);
+            break;
+          }
+          case  1 : {
+            console.log(`INSERT: ${text}`);
+            typeRef.current = typeRef.current.type(text);
+            break;
+          }
+          case -1 : {
+            console.log(`DELETE: ${text}`);
+            typeRef.current = typeRef.current.move(text.length).delete(text.length);
+            break;
+          }
+          default : {
+            break;
+          }
+        }
+      }
+
+      typeRef.current = typeRef.current.exec(() => {
+        typeRef.current.freeze()
+      })
+      prev = curr;
+    }
+
+    typeRef.current.go();
+    if (typeRef.current.is('completed')) {
+      console.log('destroying typeit instance')
+      typeRef.current.destroy();
+    }
+
+  }, [strings])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <pre>
+      <code ref={codeRef} />
+    </pre>
+  )
+
+})
+
+export default function App() {
+  const typeRef = useRef(null);
+  const strings = [
+    `function foo(x) {
+  return x + 1;
+}`,
+    `function bar(self, y) {
+  return y + 1 * 2;
+}`,
+    `function recursive_bar(y) {
+  return recursive_bar(y + 1);
+}`,
+  ]
+
+  return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={() => {typeRef.current.step()}}> Step </button>
+        <CodeTyper strings={strings} ref={typeRef} />
+      </div>
   );
 }
-
-export default App;
